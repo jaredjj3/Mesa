46ed6d8f148087af837799e6356b5a1dfa9f33fb
Jared Johnson <Jared@Jareds-MacBook-Pro.local>
2016-10-18 12:15:34 -0400

    add gitignore, start rails project to prove that Mesa works

--- test/cat.rb
+++ test/cat.rb
@@ -4,11 +4,14 @@ class Cat < Mesa
 
 end
 
-puts "Cat.all"
-p Cat.all
+system('clear')
+puts "Cat.all =>"
+cats = Cat.all
+print cats.map{ |cat| cat.attributes }.join("\n")
 puts
-puts "Cat.find(1)"
-puts Cat.find(1)
 puts
-puts "Cat.where(name: 'Breakfast')"
-puts Cat.where(name: 'Breakfast')
+puts "Cat.find(1) =>"
+print Cat.find(1).attributes.to_s + "\n"
+puts
+puts "Cat.where(name: 'Breakfast') =>"
+print Cat.where(name: 'Breakfast').first.attributes.to_s + "\n"
